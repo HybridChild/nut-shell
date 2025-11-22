1b411e4266cf68da02458adb19284183d28d0d13
HybridChild <esbendn@tutanota.com>
2025-11-22 08:59:50 +0100

    Fix unused imports warnings

--- tests/test_shell_integration.rs
+++ tests/test_shell_integration.rs
@@ -9,7 +9,9 @@
 #[path = "fixtures/mod.rs"]
 mod fixtures;
 
+#[allow(unused_imports)]
 use fixtures::{MockHandlers, MockIo, TEST_TREE};
+#[allow(unused_imports)]
 use nut_shell::Shell;
 
 // ============================================================================
